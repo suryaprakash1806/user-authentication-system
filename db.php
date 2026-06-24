0eb75830b1b4fe4444d4b5929347af262251f34e
suryaprakash1806 <vsuryagn019@gmail.com>
2026-06-24 19:43:16 +0530

    Update db.php
    Task 3: Updated db.php with database error handling

--- db.php
+++ db.php
@@ -1,14 +1,24 @@
 <?php
 
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "user_auth";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection Failed: " . $conn->connect_error);
+try
+{
+    $conn = new mysqli(
+        $servername,
+        $username,
+        $password,
+        $dbname
+    );
+}
+catch(Exception $e)
+{
+    die("Database Connection Failed");
 }
 
 ?>
